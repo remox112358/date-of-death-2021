bca4c75ec5df2bc2a87ee2be6f472025cd333f09
remox112358 <remox112358@gmail.com>
2021-08-16 14:48:30 +0400

    wip: directories structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,33 @@
 <template>
   <router-view />
 </template>
+
+<style lang="scss">
+
+  @import './scss/fonts.scss';
+  @import './scss/reset.scss';
+  @import './scss/variables.scss';
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+
+    overflow-x: hidden;
+
+    font-size: 14px;
+    font-style: normal;
+    font-weight: normal;
+    font-family: $font--roboto;
+
+    background-color: $color--background;
+  }
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+
   state: {
+    // ...
   },
+
   mutations: {
+    // ...
   },
+
   actions: {
+    // ...
   },
+
   modules: {
-  }
+    // ...
+  },
+
 })
