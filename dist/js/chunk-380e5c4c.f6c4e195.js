16b9fe569eb5da7fe097f44c538e799ca3f04330
remox112358 <remox112358@gmail.com>
2021-08-19 16:53:00 +0400

    wip: build

--- dist/js/chunk-380e5c4c.f6c4e195.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-380e5c4c"],{9245:function(t,e,c){"use strict";c.r(e);var n=c("7a23");function o(t,e){var c=Object(n["D"])("router-view");return Object(n["x"])(),Object(n["f"])("div",{class:Object(n["q"])(t.styles.root)},[Object(n["i"])(c)],2)}const r={};r.render=o;var s=r,u=c("c058"),i=c.n(u);e["default"]={extends:s,setup:function(){return{styles:i.a}}}},c058:function(t,e,c){t.exports={root:"style-module_root_1Zcbn"}}}]);
-//# sourceMappingURL=chunk-380e5c4c.f6c4e195.js.map
